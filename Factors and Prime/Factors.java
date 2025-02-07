bfddb37c45fe8dba9485d0936e6cadfcfb6beb4f
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-08 01:20:01 +0530

    New Commit

---
+++ Factors and Prime/Factors.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Factors
+{
+	public static void main(String[] args)
+	{
+	Scanner ab = new Scanner(System.in);
+	System.out.print("Enter the number : ");
+	int num = ab.nextInt();
+	int cnt = 0;
+	for (int i=1; i<=num; i++ )
+	{
+		if (num%i==0)	
+		{
+			cnt++;
+			System.out.print(i+" ");
+		}
+		
+	}
+	System.out.println();
+    System.out.print("Total factors are : "+cnt);
+	}
+}
