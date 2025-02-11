f5fb0d290cf08e0fe0712c50adcbca584d2f77dd
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-11 11:08:42 +0530

    New Code Added

---
+++ FrequencyOfDigit/FrequencyOfDigit1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class FrequencyOfDigit1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nEnter a number : ");
+		long num = sc.nextLong();
+		int digit = 0;
+		int max = 0;
+		for (int i = 0; i<=9 ; i++)
+		{
+			int cnt = 0;
+			for (long j=num; j>0 ;j/=10 )
+			{
+				long rem = j%10;
+				if (i==rem)
+				{
+					cnt++;
+				}
+			}
+			if (cnt!=0)
+			{
+				if (max<cnt)
+				{
+					max = cnt;
+					digit = i;
+				}
+			}
+		}
+		System.out.println("Highest Repeating digit is "+digit+" with a freqency of "+max);
+	}
+}
