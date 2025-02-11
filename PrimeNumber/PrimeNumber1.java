095f579bc75eb25e0e46e0a9e6669f35edff691b
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-11 12:05:04 +0530

    commit-1

---
+++ PrimeNumber/PrimeNumber1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class PrimeNumber1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("<--- Check Prime Number --->");
+		System.out.print("Enter a number : ");
+		int num = sc.nextInt();
+		int den = 2;
+		if (num< 2)
+		{
+           System.out.println(num+" it is not Prime Number");
+		}
+		else{
+		while (den<=num/2)//(den<num) method2
+		{
+			if (num%den==0) 
+			{
+				break;
+			}
+			den++;
+		}
+		if (den>num/2) //(num==den) method2
+		{
+			System.out.println(num+" it is Prime Number");
+		}
+		else
+		{
+			System.out.println(num+" it is not Prime Number");
+		}
+		}
+	}
+}
