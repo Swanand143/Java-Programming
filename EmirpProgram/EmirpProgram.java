117235c19c00442789c44210cd94165692bd6cf9
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-12 10:57:52 +0530

    Commit-4
    Emirp Program Added

---
+++ EmirpProgram/EmirpProgram.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class EmirpProgram
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n<---- Emirp Program ---->\n");
+		System.out.print("Enter a number : ");
+		int num = sc.nextInt();
+		int dup = num;
+		int den= 2, den2 = 2;
+		int rem = 0, rev = 0;
+		if (num<2)
+		{
+			System.out.println(num+" is not a prime number");
+		}
+		else
+		{
+			while(num>0)
+			{
+				rem = num%10;
+				rev = (rev*10)+rem;
+				num/=10;
+			}
+			System.out.println("Reverse Number : "+rev);
+			while (den<=dup/2)
+			{
+				if (dup%den==0)
+				{
+					break;
+				}
+				den++;
+			}
+			while (den2<=rev/2)
+			{
+				if (rev%den2==0)
+				{
+					break;
+				}
+				den2++;
+			}
+			if (den>dup/2 && den2>rev/2)
+			{
+				System.out.println(dup+" is a Emirp number");
+			}
+			else if (den>dup/2)
+			{
+                System.out.println(dup+" is prime number but not Emirp number");
+			}
+			else
+			{
+				System.out.println(dup+" is not a prime number");
+			}	
+		}
+	}
+}
