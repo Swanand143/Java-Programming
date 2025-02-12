89bdfbb254851c71fc6982446cf23686f965a97b
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-13 03:27:45 +0530

    Commit-3
    Print Series 3 added

---
+++ PrintSeries/PrintSeries3.java
@@ -0,0 +1,21 @@
+// Fibonacci Series 0 1 1 2 3 5 8 13
+import java.util.Scanner;
+class PrintSeries3
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a range : ");
+		int range = sc.nextInt();
+		int n1 = 0;
+		int n2 = 1;
+		System.out.print(n1+" "+n2+" ");
+		for (int i=2; i<range; i++)
+		{
+			int next = n1 + n2;	
+			System.out.print(next+" ");
+			n1 = n2;
+			n2 = next;
+		}
+	}
+}
