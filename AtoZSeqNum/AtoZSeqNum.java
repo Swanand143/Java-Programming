78b4420d552ddd06f4905abdad642980e1480e86
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-12 11:37:41 +0530

    Commit-5

---
+++ AtoZSeqNum/AtoZSeqNum.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class AtoZSeqNum
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n<--- Print A to Z Sequence Number --->\n");
+		System.out.print("Enter Character : ");
+		String str = sc.next().toUpperCase();
+		int len = str.length();
+		int value = 0;
+		boolean flag = true;
+
+        for (int i = 0; i < len; i++) 
+		{
+            if (str.charAt(i) < 'A' || str.charAt(i) > 'Z') 
+			{
+                flag = false;
+                break;
+            }
+        }
+        if (!flag||len>2) 
+		{
+            System.out.println("Enter a valid character (A-Z only or Max 2 letters(AA)).");
+		}
+		else if(len==1)
+		{
+			value = str.charAt(0)-64;
+			System.out.println(str+" : "+value);
+		}
+		else if (len==2)
+		{
+			value = ((str.charAt(0)-64)*26)+(str.charAt(1)-64);
+			System.out.println(str+" : "+value);
+		}
+	}
+}
