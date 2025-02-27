82fd8200415a561ce3a118f5549c8c1e27b71876
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-27 22:36:13 +0530

    Method Program 5 Added

---
+++ Tejus Sir/Methods/M5.java
@@ -0,0 +1,14 @@
+class M5
+{
+	public static void main(String[] args) 
+	{
+		add(10,20);//30
+		add(123,24);//147
+		add(12/2,34);//40
+	}
+	public static void add(int a,int b)
+	{
+		int res = a+b;
+		System.out.println(res);
+	}
+}
