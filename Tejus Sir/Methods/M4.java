833d9f859a07911db5460a8c0c833cebee95fda6
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-27 22:27:18 +0530

    Method Program 4 Added

---
+++ Tejus Sir/Methods/M4.java
@@ -0,0 +1,14 @@
+class M4
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		System.out.println(a);//10
+		test();
+	}
+	public static void test()
+	{
+		String a = "Qspiders";
+		System.out.println(a);//Qspiders
+	}
+}
