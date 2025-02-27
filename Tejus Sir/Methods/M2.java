486cfd8d81db973d0a347a6c404bd11e72b1b97e
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-27 22:25:42 +0530

    Method Program 2 Added

---
+++ Tejus Sir/Methods/M2.java
@@ -0,0 +1,19 @@
+class M2
+{
+	public static void main(String[] args) 
+	{
+		raju();
+		raju();
+		raju();
+		chutki();
+		chutki();
+	}
+	public static void raju()
+	{
+		System.out.println("Hey I'am Raju");
+	}
+	public static void chutki()
+	{
+		System.out.println("Hey I'am Chutki");
+	}
+}
