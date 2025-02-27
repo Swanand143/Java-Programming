89da9cf3f34d1783d7fa03ab51d4ed58879abb58
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-27 17:39:53 +0530

    Method 1 Program Added

---
+++ Tejus Sir/Methods/M1.java
@@ -0,0 +1,12 @@
+class M1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From main method");
+		test();//method call statement
+	}
+	public static void test()
+	{
+		System.out.println("From test method");
+	}
+}
