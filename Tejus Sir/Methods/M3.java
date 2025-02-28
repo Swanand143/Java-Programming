f3c12dd1eb5f062bd1c1daf59cf5eb5cc928194e
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-02-28 22:16:10 +0530

    In Methods ReturnType Program 1 Added

--- Tejus Sir/Methods/M3.java
+++ Tejus Sir/Methods/M3.java
@@ -3,7 +3,7 @@ class M3
 	public static void main(String[] args) 
 	{
 		int a = 10;
-		System.out.println(a);
+		System.out.println(a);//10
 		test();
 	}
 	public static void test()
---
+++ Tejus Sir/Methods/ReturnType/Return1.java
@@ -0,0 +1,12 @@
+class Return1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(mergeName("Swanand","Gholap"));
+	}
+	public static String mergeName(String fname,String lname)
+	{
+		String ans = fname+" "+lname;
+		return ans;
+	}
+}
