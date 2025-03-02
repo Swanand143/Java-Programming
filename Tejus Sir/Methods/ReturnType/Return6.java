edcfb2b6647f78d63ed5476cae6ce2799eb11152
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-02 19:10:19 +0530

    ReturnType Program 6 Added

---
+++ Tejus Sir/Methods/ReturnType/Return6.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Return6
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int num = sc.nextInt();
+		System.out.println("Rotated Number : "+rotate(num));	
+	}
+	public static int rotate(int num)
+	{
+		int last = num%10;
+		num/=10;
+		int ct = count(num);
+		return last * power(10,ct)+num;
+		//return (num%10)*power(10,(count(num)-1))+(num/10);
+	}
+	public static int count(int num)
+	{
+		int ct = 0;
+		while(num!=0)
+		{
+			num/=10;
+			ct++;
+		}
+		return ct;
+	}
+	public static int power(int digit,int ct)
+	{
+		int pow = 1;
+		for (int i=0; i<ct; i++) 
+		{
+			pow*=digit;
+		}
+		return pow;
+	}
+}
