94f642123b936c893c112b61014fca985d44c439
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-04 22:41:34 +0530

    Recursion of reversing a number program added

---
+++ Tejus Sir/Methods/Recursion/RecReverse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class RecReverse
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Enter a number : ");
+		int num = new Scanner(System.in).nextInt();
+		reverse(num,0);
+	}
+	public static void reverse(int num, int rev)
+	{
+		if (num!=0) 
+		{
+			rev = rev*10+(num%10);
+			reverse(num/10,rev);
+		}
+		else if (num==0) 
+		{
+			System.out.println(rev);
+		}
+		return;
+	}
+}
