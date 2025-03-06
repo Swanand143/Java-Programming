00116602f5ccb27bf7ee6fdf557dd48c55e6f0dc
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-06 12:59:00 +0530

    Pascal Triangle program added

---
+++ Tejus Sir/Methods/NumberType/PascalTriangle.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class PascalTriangle
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter num : ");
+	    int num = sc.nextInt();
+		System.out.println();
+		pascalTriangle(num);
+	}
+	public static void pascalTriangle(int n)
+	{
+		for (int i=0; i<n; i++)
+		{
+			for (int space=0; space<n-i-1; space++)
+			{
+				System.out.print(" "+" ");
+			}
+			for (int star=0; star<=i; star++)
+			{
+				System.out.print(fact(i)/(fact(star)*fact(i-star))+"   ");
+			}
+			System.out.println();
+		}
+	}
+	public static int fact(int num)
+	{
+		int fact = 1;
+		for (int i=num; i>0; i--)
+		{
+			fact*=i;
+		}
+		return fact;
+	}
+}
