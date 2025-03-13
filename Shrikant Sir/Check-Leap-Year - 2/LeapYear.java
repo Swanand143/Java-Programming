0a52b55d58ca82f127127377d68f5ce5a7109e91
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-13 23:02:51 +0530

    Practice Program Added

---
+++ Shrikant Sir/Check-Leap-Year - 2/LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class LeapYear2
+{
+	public static void main(String[] args)
+	{
+		Scanner ab = new Scanner(System.in);
+		System.out.print("Enter The Year : ");
+		int year = ab.nextInt();
+		if ((year%4 == 0 && year%100 != 0 )||(year%400 == 0))
+		{
+          System.out.print(year+" it is a Leap Year");
+		}
+		else
+		{
+		  System.out.print(year+" it is a not Leap Year");	
+		}
+	}
+}
