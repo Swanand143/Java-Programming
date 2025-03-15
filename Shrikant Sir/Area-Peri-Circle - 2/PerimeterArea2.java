6a6617fb80f286aac9735461e225a1d108a90ee1
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-15 21:32:18 +0530

    Update PerimeterArea2.java

--- Shrikant Sir/Area-Peri-Circle - 2/PerimeterArea2.java
+++ Shrikant Sir/Area-Peri-Circle - 2/PerimeterArea2.java
@@ -8,12 +8,5 @@ class PerimeterArea2
 		System.out.print("Enter the radius(cm): ");
 		float radius = ab.nextFloat();
 		final float pi = 22/7f;
-		float area = pi *(radius*radius);
-		System.out.println("Area of circle is : "+area+"cm^2") ;
-
-		float Perimeter = 2* pi *radius;
-		System.out.println("Perimeter of circle is : "+Perimeter+"cm");
-
-
 	}
 }
