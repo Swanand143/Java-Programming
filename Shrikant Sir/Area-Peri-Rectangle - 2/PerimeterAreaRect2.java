a65633cacffc942c06330e136dcd2297fe98e848
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-17 10:55:19 +0530

    Update PerimeterAreaRect2.java

--- Shrikant Sir/Area-Peri-Rectangle - 2/PerimeterAreaRect2.java
+++ Shrikant Sir/Area-Peri-Rectangle - 2/PerimeterAreaRect2.java
@@ -4,9 +4,7 @@ class PerimeterAreaRect2
   public static void main (String[] args)
 	{
 
-		Scanner ab = new Scanner(System.in);
-		System.out.print("Enter the length(cm): ");
-		float length = ab.nextFloat();
+		
 
 		System.out.print("Enter the breadth(cm): ");
 		float breadth = ab.nextFloat();
