4bfd6361012c7af8e120b7fa2622204115979337
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-17 10:54:39 +0530

    Update PerimeterArea2.java

--- Shrikant Sir/Area-Peri-Circle - 2/PerimeterArea2.java
+++ Shrikant Sir/Area-Peri-Circle - 2/PerimeterArea2.java
@@ -4,9 +4,6 @@ class PerimeterArea2
   public static void main (String[] args)
 	{
 
-		Scanner ab = new Scanner(System.in);
-		System.out.print("Enter the radius(cm): ");
-		float radius = ab.nextFloat();
-		final float pi = 22/7f;
+		
 	}
 }
