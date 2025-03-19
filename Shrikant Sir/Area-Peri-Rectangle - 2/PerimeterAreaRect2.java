c7d8b47268a21052a12ea6c8dddfa0ca60a2c61b
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-19 21:19:57 +0530

    Update PerimeterAreaRect2.java

--- Shrikant Sir/Area-Peri-Rectangle - 2/PerimeterAreaRect2.java
+++ Shrikant Sir/Area-Peri-Rectangle - 2/PerimeterAreaRect2.java
@@ -3,11 +3,6 @@ class PerimeterAreaRect2
 {
   public static void main (String[] args)
 	{
-
-		
-
-		System.out.print("Enter the breadth(cm): ");
-		float breadth = ab.nextFloat();
 		
 		float area = length*breadth;
 		System.out.println("Area of rectangle is : "+area+"cm^2") ;
