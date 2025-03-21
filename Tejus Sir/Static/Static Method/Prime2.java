297ec5417c6552a908044bab51369114f92e8c85
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-21 22:25:48 +0530

    Static Method Added

---
+++ Tejus Sir/Static/Static Method/Prime2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Prime2{
+	public static void main(String[] args) {
+		System.out.print("Enter number to check : ");
+		int num = new Scanner(System.in).nextInt();
+		if (isPrime(num , 2)) {
+			System.out.print(num + " is a prime number");
+		}
+		else 
+			System.out.print(num +" is not a prime number");
+	}
+	public static boolean isPrime(int num , int den )
+	{
+		//normal method 
+		/*if (den == num/2) {
+			return true;
+		}
+		if (num%den == 0) {
+			return false;
+		}
+		return isPrime(num , ++den);*/
+
+	// optimise method 
+	 return den == num/2 ? true : (num% den == 0 ? false : isPrime(num , ++den));
+	}
+}
