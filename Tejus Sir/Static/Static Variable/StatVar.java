52adfdf4d4f5ac1a8264ddecd5cdbb33f44b88c7
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-22 13:59:16 +0530

    Static Variable Program 3 Added

---
+++ Tejus Sir/Static/Static Variable/StatVar.java
@@ -0,0 +1,25 @@
+class StatVar 
+{
+	static byte a ;//0
+	static short b ;//0
+	static char c;// blank space
+	static int d ;//0
+	static long e ;//0
+	static float f ;//0.0
+	static double g ;//0.0
+	static boolean h ;//false
+	static String i ;//null
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("byte : "+a);
+		System.out.println("short : "+b);
+		System.out.println("char : "+c);
+		System.out.println("int : "+d);
+		System.out.println("long : "+e);
+		System.out.println("float : "+f);
+		System.out.println("double : "+g);
+		System.out.println("boolean : "+h);
+		System.out.println("string : "+i);
+	}
+}
