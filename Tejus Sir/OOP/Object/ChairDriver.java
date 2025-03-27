354c6b10b0ef31cfb442a3bdc9fa25738c91a170
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-03-27 10:30:47 +0530

    Object Creation Program 1 Added

---
+++ Tejus Sir/OOP/Object/ChairDriver.java
@@ -0,0 +1,11 @@
+class ChairDriver
+{
+	public static void main(String[] args)
+	{
+		Chair ch1 = new Chair();
+		System.out.println(ch1);
+		
+		Chair ch2 = new Chair();
+		System.out.println(ch2);
+	}
+}
