cdae80ae752c2471a6709d0f9a7551354b22ffc0
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-04-05 13:31:03 +0530

    Changes Added

---
+++ Tejus Sir/OOP/Basic Examples/Public.java
@@ -0,0 +1,23 @@
+class D 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From Class D");
+	}
+}
+
+public class Public
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From Class Public");
+	}
+}
+
+class E
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From Class E");
+	}
+}
