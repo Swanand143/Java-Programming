a5a4ce838e62079ba5306d1bcf30926549ecc99f
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-04-07 21:59:22 +0530

    Programming Changes added

--- Shrikant Sir/FacinatingNumber - 2/FascinatingNumber.java
+++ Shrikant Sir/FacinatingNumber - 2/FascinatingNumber.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
-class FascinatingNumber2 
+class FascinatingNumber 
 {
 	// fascinating number check using string
 	public static void main(String[] args) 
 	{
 		System.out.print("Enter Number : ");
-		System.out.print("Enter Number : ");
 		int num = new Scanner(System.in).nextInt();
 		String numstr = num + ""+num*2+"" +num*3;
 		boolean flag=true;
-		
+		/*
+			fascinating number is the number :-->
+			1) multiply  - with 2
+			2) multiply - with 3
+			3) concatenate original num and other multipications. 
+			check every digit repeated only once and number should contain all digits from 1 to 9.
+		*/
 		for (char i = '1';i<='9' ;i++ )
 		{
 			int cnt = 0;
@@ -27,6 +32,8 @@ class FascinatingNumber2
 					break;
 				}
 		}
-		
+		System.out.print(flag?
+			"Yes " + num + " is a fascinating number"
+			:"No"+ num + " is not a fascinating number");
 	}
 }
