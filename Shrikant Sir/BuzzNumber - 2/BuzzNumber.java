988012360e79bff135ac462ca083989408d9e535
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-04-12 22:02:55 +0530

    Update BuzzNumber.java

--- Shrikant Sir/BuzzNumber - 2/BuzzNumber.java
+++ Shrikant Sir/BuzzNumber - 2/BuzzNumber.java
@@ -3,10 +3,7 @@ class BuzzNumber
 {
 	public static void main(String[] args) 
 	{
-		/*
-			a number divisible by 7 or having 7 at the
-			last (unit place) is called Buzz Number
-		*/
+		
 		System.out.print("Enter Number :");
 		int num = new Scanner(System.in).nextInt();
 		if (num%7==0||num%10==7)
