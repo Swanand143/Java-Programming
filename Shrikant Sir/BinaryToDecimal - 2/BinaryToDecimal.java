9f2ed11d4baed091cd34d7059be9f8c16c5cc413
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-04-12 22:03:46 +0530

    Update BinaryToDecimal.java

--- Shrikant Sir/BinaryToDecimal - 2/BinaryToDecimal.java
+++ Shrikant Sir/BinaryToDecimal - 2/BinaryToDecimal.java
@@ -7,7 +7,7 @@ class BinaryToDecimal2
 		System.out.print("Enter a  binary number : " );
 		String bin = new Scanner(System.in).next();         //"101010111";
 		int num = Integer.parseInt(bin,2);
-		System.out.print(num);
+		;
 		System.out.print(num)
 
 	}
