c44dd7900c583c00a1636639c5c0e5742a0dd6fa
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-04-15 22:01:41 +0530

    Update FrequencyOfDigit.java

--- Shrikant Sir/FrequencyOfDigit - 2/FrequencyOfDigit.java
+++ Shrikant Sir/FrequencyOfDigit - 2/FrequencyOfDigit.java
@@ -4,7 +4,7 @@ class FrequencyOfDigit
 	public static void main(String[] args)
 	{
 		System.out.print("Enter a number : ");
-		long num = sc.nextLong();
+		
 		for (int i = 0; i<=9 ; i++)
 		{
 			int cnt = 0;
