f66554356ad825bccd392293968731c46e4ac06e
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-04-23 18:22:05 +0530

    Update CheckCharacter.java

--- Shrikant Sir/Check-Character - 2/CheckCharacter.java
+++ Shrikant Sir/Check-Character - 2/CheckCharacter.java
@@ -7,7 +7,7 @@ class CheckCharacter2
 		char ch = new Scanner(System.in).next().charAt(0);
 
 		String ans = (ch>='A' && ch<='Z'||ch>='a' && ch<='z')?((ch>='A' && ch<='Z')?(ch+" is a UpperCase Alphabet"):(ch+" is a LowerCase Alphabet")):((ch>='0'&& ch<='9')?(ch+" it is a digit"):(ch+" it is a Special Character"));
-		;
+		
 
 	}
 }
