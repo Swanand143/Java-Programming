8e04c19b8f3f05918f12c06c16bed1f681b0daa9
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-06-30 22:41:08 +0530

    Finally Block Program Added

---
+++ Tejus Sir/Exception And Exception Handling/src/exceptionhandling/FinallyBlock.java
@@ -0,0 +1,34 @@
+package exceptionhandling;
+
+import java.util.Scanner;
+
+public class FinallyBlock {
+	
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the first num : ");
+		int num = sc.nextInt();
+		
+		System.out.print("Enter the second num : ");
+		int den = sc.nextInt();
+		
+		try {
+			
+			int ans = num/den;
+			System.out.println("Ans : "+ans);
+			
+		} 
+		catch (ArithmeticException ref) {
+			
+			System.out.println("Don't divide by zero");
+		}
+		finally {
+			
+			System.out.println("Thank You");
+		}
+		
+	}
+
+}
