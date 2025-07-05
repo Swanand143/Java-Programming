55728a9324a0737501e5e401dc8ce56c1c4e1857
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-07-05 13:50:45 +0530

    Wrapper Class Program 3 Added

---
+++ Tejus Sir/WrapperClasses/src/wrapper/Wrap3.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class Wrap3 {
+	
+	public static void main(String[] args) {
+		
+		Byte b1 = new Byte((byte)100);
+	    Integer b2 = new Integer(100);
+		System.out.println(b1.equals(b2));//not possible as they are different objects but we get o/p false
+	}
+
+
+}
