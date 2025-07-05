9c00fa7e38c575de52b36fcb16c9adf1fecaf038
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-07-05 13:26:29 +0530

    Wrapper Class Boxing Program Added

---
+++ Tejus Sir/WrapperClasses/src/wrapper/Wrap1.java
@@ -0,0 +1,16 @@
+package wrapper;
+
+public class Wrap1 {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		Integer i = new Integer(a);// Boxing
+		System.out.println(i.toString());
+		
+		Double d = new Double(10.145); //Boxing
+		System.out.println(d);
+		
+	}
+
+}
