af22bd72ea00f9191652d37633f535432fadb402
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-07-06 11:56:47 +0530

    Wrapper Classes Program 4 Added

---
+++ Tejus Sir/WrapperClasses/src/wrapper/Wrap4.java
@@ -0,0 +1,32 @@
+package wrapper;
+
+public class Wrap4 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_EXPONENT);
+		System.out.println(Double.MIN_EXPONENT);
+		
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_EXPONENT);
+		System.out.println(Float.MIN_EXPONENT);
+		
+		System.out.println(Character.MAX_VALUE);
+		System.out.println(Character.MIN_VALUE);
+	}
+}
