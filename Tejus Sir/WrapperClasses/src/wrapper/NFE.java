c46636569242e25ffcf71a5d659e8bc1c6efc5ac
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-07-07 21:01:39 +0530

    NumberFormatException Program Added

---
+++ Tejus Sir/WrapperClasses/src/wrapper/NFE.java
@@ -0,0 +1,40 @@
+package wrapper;
+
+public class NFE {
+	
+	public static void main(String[] args) {
+		
+//		String a = "36745hdfuj45";
+//		int i = Integer.parseInt(a);//NumberFormatException
+//		System.out.println(i);
+		
+//		String b = "36745hdfuj45";
+//		short s = Short.parseShort(b);//NumberFormatException
+//		System.out.println(s);
+		
+//		String c = "36745hdfuj45";
+//		byte b1 = Byte.parseByte(c);//NumberFormatException
+//		System.out.println(b1);
+		
+//		String d = "36745hdfuj45";
+//		long l = Long.parseLong(d);//NumberFormatException
+//		System.out.println(l);
+		
+//		String e = "36745hdfuj45";
+//		double d1 = Double.parseDouble(e);//NumberFormatException
+//		System.out.println(d1);
+		
+//		String f = "36745hdfuj45";
+//		float f1 = Float.parseFloat(f);//NumberFormatException
+//		System.out.println(f1);
+		
+		String g = "36745hdfuj45";
+		boolean b1 = Boolean.parseBoolean(g);//always gives o/p false
+		System.out.println(b1);
+		
+		String h = "36745hdfuj45";
+		System.out.println(h.charAt(0));
+		
+	}
+
+}
