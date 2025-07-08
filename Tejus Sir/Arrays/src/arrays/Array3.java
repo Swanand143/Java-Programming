cb59cddec4d64617b1fc543742117bd647989871
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-07-08 11:30:47 +0530

    Store Element in Arrauy Program 1 Added

---
+++ Tejus Sir/Arrays/src/arrays/Array3.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.Iterator;
+
+public class Array3 {
+	
+	public static void main(String[] args) {
+		
+		int[] arr = new int[4];
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		
+		System.out.println("Array Size = "+arr.length);// fetch the size of an array
+		
+		for(int index=0; index<arr.length;index++)
+		{
+			System.out.println(arr[index]);
+		}
+	}
+}
