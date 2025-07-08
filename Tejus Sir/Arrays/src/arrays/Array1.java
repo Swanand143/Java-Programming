22593cec304de04ee7ba537885b477eca8b07821
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-07-08 11:07:28 +0530

    First Program Of Arrays Added

---
+++ Tejus Sir/Arrays/src/arrays/Array1.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class Array1 {
+	
+	public static void main(String[] args) {
+		
+		int [] ar1 = new int[5];
+		System.out.println(ar1);// print the reference of that array
+		
+		String [] ar2 = new String[3];
+		System.out.println(ar2);
+		
+		double [] ar3 = new double[4];
+		System.out.println(ar3);
+		
+		boolean [] ar4 = new boolean[2];
+		System.out.println(ar4);
+	}
+
+}
