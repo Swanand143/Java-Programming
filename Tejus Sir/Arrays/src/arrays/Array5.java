0004b4d577da3b086c0e300e663d6beed7a8291b
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-07-09 11:55:36 +0530

    Another Way Of Creating Array Program

---
+++ Tejus Sir/Arrays/src/arrays/Array5.java
@@ -0,0 +1,21 @@
+package arrays;
+//Another way of creating an array object
+public class Array5 {
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,4,5,6};
+		for (int i = 0; i < arr.length; i++) {
+			
+			System.out.println(arr[i]);
+			
+		}
+		
+		String[] arr2 = {"Hari","Cary","Mary","Sunny"};
+		for (int i = 0; i < arr2.length; i++) {
+			
+			System.out.println(arr2[i]);
+			
+		}
+	}
+}
