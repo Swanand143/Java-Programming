73d1086984c388de974f219f2b30cd109ae88e88
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-07-11 22:10:40 +0530

    Merging of two arrays in one array program

---
+++ Tejus Sir/Arrays/src/arraysprogram/MergeTwoArrays.java
@@ -0,0 +1,45 @@
+package arraysprogram;
+
+import java.util.Arrays;
+
+public class MergeTwoArrays {
+	
+	public static void main(String[] args) {
+		
+		int[] arr1 = {10,20,30,40,50};
+		int[] arr2 = {60,70,80,90,100};
+		System.out.println("Array 1 = "+Arrays.toString(arr1));
+		System.out.println("Array 2 = "+Arrays.toString(arr2));
+		System.out.println("Merged Array = "+Arrays.toString(mergeArray(arr1, arr2)));
+	}
+	
+	//Method 1
+	public static int[] mergeArray(int[]arr1,int[]arr2)
+	{
+		int mem = 0;
+		int[] ans = new int[arr1.length+arr2.length]; 
+		for (int i = 0; i < arr1.length; i++,mem++) {
+			ans[mem] = arr1[i];
+		}
+		for (int i = 0; i < arr2.length; i++,mem++) {
+			ans[mem] = arr2[i];
+		}
+		return ans;
+	}
+	//Method 2
+//	public static int[] mergeArray(int[]arr1,int[]arr2)
+//	{
+//		int[] ans = new int[arr1.length+arr2.length];
+//		for (int i = 0; i < ans.length; i++) {
+//			if (i<arr1.length) {
+//				ans[i] = arr1[i];
+//			} else {
+//				ans[i] = arr2[i-arr1.length];
+//			}
+//			
+//		}
+//		return ans ;
+//	}
+	
+
+}
