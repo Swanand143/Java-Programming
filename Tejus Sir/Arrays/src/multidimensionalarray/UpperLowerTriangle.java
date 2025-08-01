19f5fd0c0287f01c818bb9f0593cdd26517b19da
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-08-01 10:23:36 +0530

    Check Whether sum above diagonal elements is equal to sum of below diagonal element

---
+++ Tejus Sir/Arrays/src/multidimensionalarray/UpperLowerTriangle.java
@@ -0,0 +1,46 @@
+package multidimensionalarray;
+
+public class UpperLowerTriangle {
+
+	public static void main(String[] args) {
+		int [][]a = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		System.out.println("Sum Of Lower Triangle : "+sumOfLowerTri(a));
+		System.out.println("Sum Of Upper Triangle : "+sumOfUpperTri(a));
+		System.out.println("Result : "+sumOfLowerUpper(a));
+	}
+	public static int sumOfUpperTri(int[][]ar) {
+		int sum = 0;
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = i+1; j < ar.length; j++) {
+				//here we run iteration where the j is bigger than i 
+				//for accessing only the upper tringle of matrix
+				//if (j>i) {
+					sum += ar[i][j];
+			//}
+			}
+		}
+		return sum;
+	}
+	public static int sumOfLowerTri(int[][]ar) {
+		int sum = 0;
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < i; j++) {
+				//here we run iteration where the j is lesser than i 
+				//for accessing only the lower tringle of matrix
+				//if (j<i) {
+					sum += ar[i][j];
+			//}
+			}
+		}
+		return sum;
+	}
+	
+	public static boolean sumOfLowerUpper(int[][]a) {
+		return sumOfUpperTri(a)==sumOfLowerTri(a);
+	}
+	
+}
