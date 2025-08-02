920df8fb25a16b06666efa9a80f9b5e04a99bece
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-08-02 11:13:51 +0530

    Print Character From Given String Program Added

---
+++ Tejus Sir/Strings/src/stringprogramms/PrintCharFromString.java
@@ -0,0 +1,13 @@
+package stringprogramms;
+
+public class PrintCharFromString {
+	
+	public static void main(String[] args) {
+		
+		String s = "India is my country";
+		for (int i = 0; i <s.length(); i++) {
+			System.out.println(s.charAt(i));
+		}
+	}
+
+}
