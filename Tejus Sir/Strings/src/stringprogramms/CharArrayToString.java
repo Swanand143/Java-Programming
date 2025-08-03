1a6b4be023fc60f53906453b45cf84965d856c23
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-08-03 10:39:19 +0530

    From Char Array Extract The String Program Added

---
+++ Tejus Sir/Strings/src/stringprogramms/CharArrayToString.java
@@ -0,0 +1,17 @@
+package stringprogramms;
+
+public class CharArrayToString {
+	
+	public static void main(String[] args) {
+		
+		char [] ch = {'a','b','c','d','e'};
+		String s = new String(ch);
+		System.out.println("Ans : "+s);
+//		String Ans = "";
+//		for (int i = 0; i < ch.length; i++) {
+//			Ans += ch[i];
+//		}
+//		System.out.println(Ans);
+		
+	}
+}
