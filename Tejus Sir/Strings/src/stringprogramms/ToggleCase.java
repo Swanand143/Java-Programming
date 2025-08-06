ac4a90287ffb1946d7a2fcb857887582ff094bce
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-08-06 10:46:02 +0530

    Convert The Given String Into Toggle Case

---
+++ Tejus Sir/Strings/src/stringprogramms/ToggleCase.java
@@ -0,0 +1,27 @@
+package stringprogramms;
+
+public class ToggleCase {
+	
+	public static void main(String[] args) {
+		
+		String s = "aBcD@eF";
+		System.out.println("Ans : "+toggleCase(s));
+	}
+	public static String toggleCase(String s)
+	{
+		String ans = "";
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch>='a' && ch<='z') {
+				ans = ans + (char)(ch-32);
+			}
+			else if (ch>='A' && ch<='Z') {
+				ans = ans + (char)(ch+32);
+			}
+			else {
+				ans += ch;
+			}
+		}
+		return ans;
+	}
+}
