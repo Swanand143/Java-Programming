3297fed13d6af5ac56671d0c8dcc43aafc301ea1
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-08-30 10:27:57 +0530

    Reverse The Given Array Program

---
+++ Tejus Sir/Arrays/src/arraysprogram/ReverseArray2.java
@@ -0,0 +1,22 @@
+package arraysprogram;
+
+import java.util.Arrays;
+
+public class ReverseArray2 {
+	
+	public static void main(String[] args) {
+		
+		int [] arr = {10,20,30,40,50};
+		System.out.println("Ans : "+Arrays.toString(rev(arr)));
+	}
+	public static int[] rev(int[] arr)
+	{
+		int[] temp = new int[arr.length];
+		for (int i = arr.length-1,j=0; i >= 0; i--) {
+			temp[j] = arr[i];
+			j++;
+		}
+		return temp;
+	}
+
+}
