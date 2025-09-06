a585d20347eec0d0bbe631dca6651042432d58bc
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-09-06 13:25:09 +0530

    Find The Duplicate Elements From An Array Program

---
+++ Tejus Sir/Arrays/src/arraysprogram/DuplicateEle.java
@@ -0,0 +1,22 @@
+package arraysprogram;
+
+public class DuplicateEle {
+	
+	public static void main(String[] args) {
+        int[] arr = {4, 2, 7, 2, 4, 9, 7};
+        findDuplicates(arr);
+    }
+	
+	public static void findDuplicates(int[] arr) {
+        System.out.println("Duplicate elements are:");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
